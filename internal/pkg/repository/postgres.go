0817d3b39ec97f09cd92488d9c2121b0c8cb7191
servady <servadyur@gmail.com>
2022-04-16 13:38:28 +0300

    added postgres realization of Repository interface

---
+++ internal/pkg/repository/postgres.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/rs/zerolog"
+	"github.com/serjyuriev/diploma-1/internal/pkg/config"
+	"github.com/serjyuriev/diploma-1/internal/pkg/models"
+)
+
+var (
+	errNotImplemented = errors.New("method not implemented yet")
+)
+
+type postgres struct {
+	cfg    config.Config
+	db     *sql.DB
+	logger zerolog.Logger
+}
+
+func NewPostgres(logger zerolog.Logger) (Repository, error) {
+	return &postgres{
+		cfg:    config.GetConfig(),
+		db:     nil,
+		logger: logger,
+	}, nil
+}
+
+func (p *postgres) InsertUser(ctx context.Context, user models.User) error {
+	return errNotImplemented
+}
+
+func (p *postgres) SelectUser(ctx context.Context, user models.User) error {
+	return errNotImplemented
+}
+
+func (p *postgres) InsertOrder(ctx context.Context, number, userID string) error {
+	return errNotImplemented
+}
+
+func (p *postgres) SelectOrdersByUser(ctx context.Context, userID string) ([]models.Order, error) {
+	return nil, errNotImplemented
+}
+
+func (p *postgres) SelectBalanceByUser(ctx context.Context, userID string) (models.Balance, error) {
+	return models.Balance{}, errNotImplemented
+}
+
+func (p *postgres) UpdateBalance(ctx context.Context, userID string, amount float64) error {
+	return errNotImplemented
+}
+
+func (p *postgres) SelectWithdrawalsByUser(ctx context.Context, userID string) ([]models.Order, error) {
+	return nil, errNotImplemented
+}
