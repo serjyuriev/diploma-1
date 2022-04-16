ee834c1bbf9c1c492b84328003dcdc2fe815f4d2
servady <servadyur@gmail.com>
2022-04-16 13:50:51 +0300

    added repo and svc fields in handlers struct

--- internal/pkg/handlers/handlers.go
+++ internal/pkg/handlers/handlers.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/rs/zerolog"
+	"github.com/serjyuriev/diploma-1/internal/pkg/repository"
+	"github.com/serjyuriev/diploma-1/internal/pkg/service"
 )
 
 type Handlers interface {
@@ -18,11 +20,29 @@ type Handlers interface {
 
 type handlers struct {
 	logger zerolog.Logger
+	repo   repository.Repository
+	svc    service.Service
 }
 
 func MakeHandlers(logger zerolog.Logger) (Handlers, error) {
+	logger.Debug().Msg("preparing handlers")
+
+	repo, err := repository.NewPostgres(logger)
+	if err != nil {
+		logger.Error().Msg("unable to initialize postgres repository")
+		return nil, err
+	}
+
+	svc, err := service.NewService(logger, repo)
+	if err != nil {
+		logger.Error().Msg("unable to initialize new service")
+		return nil, err
+	}
+
 	return &handlers{
 		logger: logger,
+		repo:   repo,
+		svc:    svc,
 	}, nil
 }
 
