65d06684cfc6b8997a1679141046a7b397730346
servady <servadyur@gmail.com>
2022-04-17 20:19:05 +0300

    added application entrypoint

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/serjyuriev/diploma-1/internal/app"
+)
+
+func main() {
+	app, err := app.NewApp()
+	if err != nil {
+		log.Fatalf("unable to initialized new app: %v", err)
+	}
+
+	if err := app.Start(); err != nil {
+		log.Fatalf("an error occured while the app was working: %v", err)
+	}
+}
