885d626137cacfa822b2f2523ecfbed2b991e50a
servady <servadyur@gmail.com>
2022-04-18 23:05:33 +0300

    implemented registration and login handlers

--- internal/pkg/handlers/handlers.go
+++ internal/pkg/handlers/handlers.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/rs/zerolog"
+	"github.com/serjyuriev/diploma-1/internal/pkg/models"
 	"github.com/serjyuriev/diploma-1/internal/pkg/repository"
 	"github.com/serjyuriev/diploma-1/internal/pkg/service"
 )
@@ -26,16 +30,18 @@ type handlers struct {
 }
 
 func MakeHandlers(logger zerolog.Logger) (Handlers, error) {
-	logger.Debug().Msg("preparing handlers")
+	logger.Debug().Caller().Msg("preparing handlers")
 
 	repo, err := repository.NewPostgres(logger)
 	if err != nil {
-		return nil, fmt.Errorf("unable to initialize postgres repository: %v", err)
+		logger.Error().Caller().Msg("unable to initialize postgres repository")
+		return nil, err
 	}
 
 	svc, err := service.NewService(logger, repo)
 	if err != nil {
-		return nil, fmt.Errorf("unable to create new service: %v", err)
+		logger.Error().Caller().Msg("unable to create new service")
+		return nil, err
 	}
 
 	return &handlers{
@@ -46,11 +52,66 @@ func MakeHandlers(logger zerolog.Logger) (Handlers, error) {
 }
 
 func (h *handlers) RegisterUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.logger.Err(err).Caller().Msg("unable to read request body")
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	user := new(models.User)
+	if err := json.Unmarshal(body, user); err != nil {
+		h.logger.Err(err).Caller().Msg("unable to map json to user")
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	token, err := h.svc.RegisterUser(r.Context(), user)
+	if err != nil {
+		if errors.Is(err, service.ErrNotRegistered) {
+			h.logger.Err(err).Caller().Msg("unable to login just registered user")
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		h.logger.Err(err).Caller().Msg("unable to register user")
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	w.WriteHeader(http.StatusOK)
 }
 
 func (h *handlers) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.logger.Err(err).Caller().Msg("unable to read request body")
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	user := new(models.User)
+	if err := json.Unmarshal(body, user); err != nil {
+		h.logger.Err(err).Caller().Msg("unable to map json to user")
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	token, err := h.svc.LoginUser(r.Context(), user)
+	if err != nil {
+		if errors.Is(err, service.ErrNotRegistered) {
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+
+		h.logger.Err(err).Caller().Msg("unable to login user")
+		http.Error(w, "internal server error", http.StatusUnauthorized)
+		return
+	}
+
+	w.Header().Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	w.WriteHeader(http.StatusOK)
 }
 
 func (h *handlers) PostUserOrderHandler(w http.ResponseWriter, r *http.Request) {
