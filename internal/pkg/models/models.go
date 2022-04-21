f3ec4a4377b8d7ebd98d59f2f1596b9d4cb6c43f
servady <servadyur@gmail.com>
2022-04-21 22:12:54 +0300

    added methods to convert from float to points and otherwise

--- internal/pkg/models/models.go
+++ internal/pkg/models/models.go
@@ -19,12 +19,23 @@ type Claims struct {
 type Order struct {
 	Number      string
 	Status      string
+	Accrual     Point
 	Sum         Point
 	UploadedAt  time.Time
 	ProcessedAt time.Time
 }
 
 type Point int64
+
+func (p Point) Float64() float64 {
+	x := float64(p)
+	x = x / 100
+	return x
+}
+
+func ToPoints(f float64) Point {
+	return Point((f * 100) + 0.5)
+}
 
 type User struct {
 	ID       int    `json:"id,omitempty"`
