ae1df6d88a00a19d419d55d747e374b832e4360f
servady <servadyur@gmail.com>
2022-04-21 22:40:59 +0300

    added log messages in cmd

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main() {
+	log.Println("creating application...")
 	app, err := app.NewApp()
 	if err != nil {
 		log.Fatalf("unable to initialized new app: %v", err)
 	}
 
+	log.Println("starting application...")
 	if err := app.Start(); err != nil {
 		log.Fatalf("an error occured while the app was working: %v", err)
 	}
